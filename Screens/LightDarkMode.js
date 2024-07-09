810a57ac66a346e2eeece07e859cd3e786e0e5be
Nicholas-Vignogna <nick.vignogna@gmail.com>
2024-07-09 16:56:38 -0400

    Completed Contact Form, and updated many UI things

--- Screens/LightDarkMode.js
+++ Screens/LightDarkMode.js
@@ -1,19 +1,24 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.getElementById("theme-toggle");
+    const darkModeSwitch = document.getElementById("darkModeSwitch");
     const currentTheme = localStorage.getItem("theme") || "light";
 
     document.body.classList.add(currentTheme + "-mode");
 
-    toggleButton.addEventListener("click", () => {
-        if (document.body.classList.contains("light-mode")) {
-            document.body.classList.remove("light-mode");
-            document.body.classList.add("dark-mode");
-            localStorage.setItem("theme", "dark");
-        } else {
-            document.body.classList.remove("dark-mode");
-            document.body.classList.add("light-mode");
-            localStorage.setItem("theme", "light");
-        }
-    });
+    if (darkModeSwitch) {
+        darkModeSwitch.checked = currentTheme === "dark";
+        darkModeSwitch.addEventListener("click", () => {
+            if (document.body.classList.contains("light-mode")) {
+                document.body.classList.remove("light-mode");
+                document.body.classList.add("dark-mode");
+                localStorage.setItem("theme", "dark");
+            } else {
+                document.body.classList.remove("dark-mode");
+                document.body.classList.add("light-mode");
+                localStorage.setItem("theme", "light");
+            }
+        });
+    } else {
+        // For pages without the dark mode switch
+        document.body.classList.add(currentTheme + "-mode");
+    }
 });
