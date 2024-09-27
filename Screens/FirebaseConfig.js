6a67d1371c9f48e29d74e2e56e91e9a01b5e59f0
Emmy2345 <123962727+Emmy2345@users.noreply.github.com>
2024-09-27 16:58:26 -0400

    Everything Fixed

--- Screens/FirebaseConfig.js
+++ Screens/FirebaseConfig.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore, collection, addDoc, deleteDoc, doc, query, where, getDocs, orderBy } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
